1964979383b9f01c6207a6ff83a3a57187764834
JKordyka <j.kordyka@ap-tech.pl>
2026-04-21 14:20:05 +0200

    vite start

--- vite.config.ts
+++ vite.config.ts
@@ -5,4 +5,12 @@ import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    watch: {
+      usePolling: true,
+    },
+    host: "0.0.0.0",
+    port: 5000,
+    strictPort: true,
+  },
 });
